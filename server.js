65a9d408a780f614a6a7db7f884e5c7711753431
Aditya Behera <124413080+adityabehera2004@users.noreply.github.com>
2023-03-30 22:57:09 -0400

    Update server.js

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ io.on('connection', socket => {
                   isDraw = true
                   for (let i = 0; i < 3; i++) {
                      for (let j = 0; j < 3; j++) {
-                        if (board[i][j] !== '') {
+                        if (board[i][j] === '') {
                            isDraw = false;
                            break;
                         }
