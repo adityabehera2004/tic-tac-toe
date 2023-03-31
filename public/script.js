8f5565466d4d13691058f035541b113ead1b75a2
Aditya Behera <124413080+adityabehera2004@users.noreply.github.com>
2023-03-30 22:56:46 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -176,7 +176,7 @@ function checkWin() {
    isDraw = true
    for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-         if (board[i][j] !== '') {
+         if (board[i][j] === '') {
             isDraw = false;
             break;
          }
